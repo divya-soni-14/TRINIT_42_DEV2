8e5e0b6bb4a39920e40d68099ab356aec44dc7c5
divya-soni-14 <ds952073@student.nitw.ac.in>
2022-01-29 17:02:46 +0400

    FINALLY WORKS

--- base/views.py
+++ base/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .models import *  # table DATA   field1(string) | field2(int)
+
+
+def home(request):
+ 
+    return render(request, "home.html")
+
+
+def login(request):
+    return render(request, "login.html")
+
+
+def register(request):
+    return render(request, "register.html")
--- core/urls.py
+++ core/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import base
+from base import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
+    path("login/", views.login, name="login"),
+    path("register/", views.register, name="register"),
 ]
